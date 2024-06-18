96c489467ab16780eff14d67162e175ff9008b04
anasommer <68922341+anasommer@users.noreply.github.com>
2024-06-18 14:12:03 +0200

    create new li func

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const itemList = document.getElementById('item-list');
 
 function addItem(e) {
   e.preventDefault();
+  const newItem = itemInput.value;
 
   // Validate Input
   if (newItem === '') {
@@ -11,7 +12,30 @@ function addItem(e) {
     return;
   }
 
-  console.log('Success');
+  // Create list item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton('remove-item btn-link text-red ');
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+
+  itemInput.value = '';
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
 }
 
 // Event Listeners
